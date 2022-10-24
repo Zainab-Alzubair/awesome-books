a3fecf5bbbd3a156850df738578413a66749f996
Shisui <37116322+Shisui6@users.noreply.github.com>
2022-10-24 17:49:40 +0100

    Implement PR changes

--- script.js
+++ script.js
@@ -1,19 +1,25 @@
+const uid = () => Date.now().toString(36) + Math.random().toString(36).substr(2);
+
+const booksSection = document.getElementById('books');
+const addBtn = document.getElementById('add-btn');
+
 let books = [
   {
-    id: 0,
+    id: uid(),
     title: 'Lorem ipsum',
     author: 'Testeroo Testyy',
   },
   {
-    id: 1,
+    id: uid(),
     title: 'Second book',
     author: 'Testeroo Testyy',
   },
 
 ];
 
-const addBook = (title, author) => {
+const addBook = (id, title, author) => {
   const book = {
+    id,
     title,
     author,
   };
@@ -26,46 +32,40 @@ const removeBook = (id) => {
   books = books.filter((book) => book.id !== id);
 };
 
-const booksSection = document.getElementById('books');
+const addRemoveListener = (id) => {
+  document.getElementById(`remove-${id}`).addEventListener('click', (e) => {
+    e.preventDefault();
+    removeBook(id);
+    const bookID = document.getElementById(`book-${id}`);
+    if (bookID.parentNode) {
+      bookID.parentNode.removeChild(bookID);
+    }
+  });
+};
 
-books.forEach((book) => {
+const appendBook = (book) => {
   const bookElement = document.createElement('div');
   bookElement.id = `book-${book.id}`;
   bookElement.innerHTML = `
     <p>${book.title}</p>
     <p>${book.author}</p>
-    <button class="remove">Remove</button>
+    <button id="remove-${book.id}">Remove</button>
     <hr>
   `;
 
   booksSection.appendChild(bookElement);
+};
+
+books.forEach((book) => {
+  appendBook(book);
+  addRemoveListener(book.id);
 });
 
-const addBtn = document.getElementById('add-btn');
 addBtn.addEventListener('click', (e) => {
   e.preventDefault();
   const bookTitle = document.getElementById('title').value;
   const bookAuthor = document.getElementById('author').value;
-  const newBook = addBook(bookTitle, bookAuthor);
-  const bookElement = document.createElement('div');
-  bookElement.innerHTML = `
-    <p>${newBook.title}</p>
-    <p>${newBook.author}</p>
-    <button class="remove">Remove</button>
-    <hr>
-  `;
-
-  booksSection.appendChild(bookElement);
+  const newBook = addBook(uid(), bookTitle, bookAuthor);
+  appendBook(newBook);
+  addRemoveListener(newBook.id);
 });
-
-const removeButtons = document.getElementsByClassName('remove');
-for (let i = 0; i < removeButtons.length; i += 1) {
-  removeButtons[i].addEventListener('click', (e) => {
-    e.preventDefault();
-    removeBook(i);
-    const bookID = document.getElementById(`book-${i}`);
-    if (bookID.parentNode) {
-      bookID.parentNode.removeChild(bookID);
-    }
-  });
-}
