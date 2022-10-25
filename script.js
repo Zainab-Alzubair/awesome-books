dc77371253f7bfa2a85ef20f1837aa99c39c4bb0
Shisui <37116322+Shisui6@users.noreply.github.com>
2022-10-25 15:14:24 +0100

    Style webpage

--- script.js
+++ script.js
@@ -1,25 +1,31 @@
 const uid = () => Date.now().toString(36) + Math.random().toString(36).substr(2);
 
-const booksSection = document.getElementById('books');
+const booksSection = document.getElementById('library');
 const addBtn = document.getElementById('add-btn');
 
-let books = [];
+let library = [];
 
-class Books {
-  constructor(id , title, author){
+class Book {
+  constructor(id, title, author) {
     this.id = id;
     this.title = title;
     this.author = author;
   }
 
-   addBook = () => {
-    books.push(this);
+  addBook = () => {
+    library.push(this);
   };
-  
-   removeBook = () => {
-    books = books.filter((book) => book.id !== this.id);
+
+  removeBook = () => {
+    library = library.filter((book) => book.id !== this.id);
   };
 }
+
+const book1 = new Book(uid(), 'Html', 'Jack');
+const book2 = new Book(uid(), 'JavaScript', 'Jane');
+
+library.push(book1);
+library.push(book2);
 
 const addRemoveListener = (book) => {
   document.getElementById(`remove-${book.id}`).addEventListener('click', (e) => {
@@ -35,21 +41,25 @@ const addRemoveListener = (book) => {
 const appendBook = (book) => {
   const bookElement = document.createElement('div');
   bookElement.id = `book-${book.id}`;
+  bookElement.className = 'book';
   bookElement.innerHTML = `
-    <p>${book.title}</p>
-    <p>${book.author}</p>
-    <button id="remove-${book.id}">Remove</button>
-    <hr>
+    <p>${book.title} by ${book.author}</p>
+    <button id="remove-${book.id}" class="remove">Remove</button>
   `;
 
   booksSection.appendChild(bookElement);
 };
 
+library.forEach((book) => {
+  appendBook(book);
+  addRemoveListener(book);
+});
+
 addBtn.addEventListener('click', (e) => {
   e.preventDefault();
   const bookTitle = document.getElementById('title').value;
   const bookAuthor = document.getElementById('author').value;
-  let book = new Books(uid(), bookTitle, bookAuthor);
+  const book = new Book(uid(), bookTitle, bookAuthor);
   book.addBook();
   appendBook(book);
   addRemoveListener(book);
